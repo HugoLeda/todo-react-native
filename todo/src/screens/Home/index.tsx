8e2ef02216ab98d81cb2e8183b265b3f0a044afa
João Hugo Leda <joaohugoleda2@gmail.com>
2022-08-24 23:49:11 -0300

    create states

--- todo/src/screens/Home/index.tsx
+++ todo/src/screens/Home/index.tsx
@@ -1,23 +1,66 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 import { styles } from './styles'
 
+import logoImg from '../../../assets/Logo.png'
+import clipboard from '../../../assets/Clipboard.png'
+
+interface task {
+  key: number;
+  description: string;
+  completed: Boolean;
+}
+
 export function Home() {
+  const [created, setCreated] = useState(0)
+  const [completed, setCompleted] = useState(0)
+  const [tasks, setTasks] = useState<task[]>([])
+
+  const addTask = () => {
+    setCreated(created + 1)    
+  }
+
+  const changeTask = () => {
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerHead}>
-        <Text>Logo</Text>
+        <Image source={logoImg}/>
       </View>  
-      <View>
+      <View style={styles.containerInput}>
         <TextInput 
           style={styles.input}
           placeholder='Adicione uma nova tarefa'
           placeholderTextColor='#808080'
         />
-        <TouchableOpacity>
-          <Text>+</Text>
+        <TouchableOpacity style={styles.button} onPress={addTask}>
+          <Text style={styles.buttonContent}>+</Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.menu}>
+        <View style={styles.menuText}>
+          <Text style={styles.textBlue}>Criadas</Text>
+          <Text style={styles.counter}>{created}</Text>
+        </View>
+        <View style={styles.menuText}>
+          <Text style={styles.textPurple}>Concluídas</Text>
+          <Text style={styles.counter}>0</Text>
+        </View>
+      </View>
+      <View style={styles.empty}>
+        <Image source={clipboard} />
+        <Text style={styles.textGrey}>
+          Você ainda não tem tarefas cadastradas
+        </Text>
+        <Text style={styles.textGrey}>
+          Crie tarefas e organize seus itens a fazer
+        </Text>
+      </View>
+      <View>
+
       </View>
     </View>
   )
--- todo/src/screens/Home/styles.ts
+++ todo/src/screens/Home/styles.ts
@@ -3,22 +3,88 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'flex-start',
     fontSize: 16,
   },
   containerHead: {
-    backgroundColor: '#000',
-    height: 173,
+    backgroundColor: '#0D0D0D',
+    height: 173,        
     alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerInput: {
+    flexDirection: 'row',
+    marginTop: -27,  
+    gap: 4,      
   },
   input: {
     backgroundColor: '#262626',
     color: '#F2F2F2',
     width: 271,
     height: 54,
-    borderRadius: 5,
-    padding: 16,    
+    borderRadius: 6,
+    padding: 16,  
+    marginRight: 4,  
   },
+  button: {
+    backgroundColor: '#1E6F9F',
+    height: 52,
+    width: 52,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',    
+  },
+  buttonContent: {
+    color: '#FFF',
+    borderColor: '#FFF',    
+    borderRadius: 50,
+    borderWidth: 1,    
+    width: 26,
+    height: 26,
+    textAlign: 'center',
+  },
+  menu: {
+    width: 327,
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 20,
+    borderBottomColor: '#262626',
+    borderBottomWidth: 1
+  },
+  menuText: {
+    flexDirection: 'row',    
+    justifyContent: 'space-between',    
+  },
+  textBlue: {
+    color: '#4EA8DE',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  textPurple: {
+    color: '#8284FA',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  counter: {
+    marginLeft: 10,
+    color: '#FFF',
+    width: 25,
+    backgroundColor: '#333333',
+    textAlign: 'center',
+    fontSize: 12,
+    fontWeight: 'bold',
+    borderRadius: 999,
+  },
+  empty: {
+    marginTop: 48,
+    alignItems: 'center',    
+  },
+  textGrey: {
+    color: '#808080',
+    fontSize: 14
+  }
 });
